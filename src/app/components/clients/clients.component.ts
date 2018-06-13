a09168cae4f6179ba64a7706ad95960523ef962d
samarth-29 <32147591+samarth-29@users.noreply.github.com>
2018-06-13 01:22:49 -0700

    Update clients.component.ts

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ClientService} from '../../services/client.service';
 
 @Component({
   selector: 'clients',
   templateUrl: './clients.component.html'
 })
-export class ClientsComponent {
- 
+export class ClientsComponent implements OnInit{
+
+  constructor(private clientService: ClientService){
+
+  }
+
+ ngOnInit(){
+   this.clientService.getClients().subscribe(clients => {
+     console.log(clients);
+   });
+ }
 }
