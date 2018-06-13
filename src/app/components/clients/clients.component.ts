f2cf84c3bbf48cf39c6ce346fe22c34f57f0d312
samarth-29 <32147591+samarth-29@users.noreply.github.com>
2018-06-13 14:33:38 -0700

    Update clients.component.ts

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -12,14 +12,17 @@ export class ClientsComponent implements OnInit{
   last_name;
   email;
   phone;
+  isEdit;
   constructor(private clientService: ClientService){
-
+  
   }
 
  ngOnInit(){
    this.clientService.getClients().subscribe(clients => {
      this.clients = clients;
    });
+
+   this.isEdit = false;
  }
 
  onAddSubmit(){
@@ -37,5 +40,37 @@ export class ClientsComponent implements OnInit{
      this.email = '';
      this.phone = '';
    });
+ }
+
+ onEditSubmit(){
+   let updClient = {
+     first_name: this.first_name,
+     last_name: this.last_name,
+     email: this.email,
+     phone: this.phone,
+     _id: this._id
+   }
+
+   this.clientService.updateClient(updClient).subscribe(client => {
+     for(let i = 0; i < this.clients.length;i++){
+       if(client._id == this.clients[i]._id){
+         this.clients.splice(i, 1);
+       }
+     }
+     this.clients.push(client);
+     this.first_name = '';
+     this.last_name = '';
+     this.email = '';
+     this.phone = '';
+   });
+ }
+
+ onEditClick(client){
+   this.isEdit = true;
+   this.first_name = client.first_name;
+   this.last_name = client.last_name;
+   this.email = client.email;
+   this.phone = client.phone;
+   this._id = client._id;
  }
 }
